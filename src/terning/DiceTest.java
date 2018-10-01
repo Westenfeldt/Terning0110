7fff34b48e165caf27a60d8a778c213c2c780261
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2018-10-01 15:20:16 +0200

    Oprettede JUnit test

---
+++ src/terning/DiceTest.java
@@ -0,0 +1,14 @@
+package terning;
+
+import static org.junit.Assert.*;
+
+public class DiceTest {
+
+    @org.junit.Test
+    public void roll() {
+    }
+
+    @org.junit.Test
+    public void rollMultiple() {
+    }
+}
