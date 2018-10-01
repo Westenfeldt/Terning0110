35dc59b0bc5f522686ded32bb94f9d3d1bff03a6
Nicolai Larsen <nicolailarsen2100@gmail.com>
2018-10-01 15:34:38 +0200

    Test af multiplerolls

--- src/terning/DiceTest.java
+++ src/terning/DiceTest.java
@@ -3,12 +3,18 @@ package terning;
 import static org.junit.Assert.*;
 
 public class DiceTest {
+Dice dice1 = new Dice();
 
     @org.junit.Test
     public void roll() {
+       int et = dice1.roll();
+       int to = dice1.roll();
+
+
     }
 
     @org.junit.Test
     public void rollMultiple() {
+        dice1.rollMultiple(2);
     }
 }
